8702479c64f3ab73b70b204c895a35fd09b989fb
eschanze <estebanschanze@gmail.com>
2024-09-16 14:15:38 -0300

    Deleted unnecessary files

--- ejercicio.py
+++
@@ -1,8 +0,0 @@
-while True:
-    cantidad = int(input("¿A cuantas personas quieres saludar? "))
-    if cantidad > 0:
-        break
-
-# Ejecutamos el for la cantidad de veces que nos dijieron en "cantidad"
-for i in range(1, cantidad + 1):
-    print(f"Saludé a Persona #{i}")
